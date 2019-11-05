6d9497f206d1070122ab83dfbcb1c467ce587aa6
Arinfo <dreydemy.florent@gmail.com>
2019-11-05 11:51:00 +0100

    ajout classe Dino

--- dinosaure.py
+++ dinosaure.py
@@ -1,6 +1,22 @@
 import pygame
 from pygame.locals import *
 
+class Dino():
+
+	def __init__(self, y):
+		self.perso = pygame.image.load("dino.png").convert_alpha() 
+		self.perso = pygame.transform.smoothscale(perso,(fond.get_width()//5,fond.get_height()//5))
+		self.y = y
+		self.saut = False
+		self.perso_x=50
+		self.perso_y=680
+		
+	def sauter(self):
+		if self.saut == True:
+			return True
+
+
+
 pygame.init()
 
 #ouverture de la fenêtre pygame.
@@ -8,7 +24,7 @@ pygame.init()
 fenetre = pygame.display.set_mode((0, 0))
 
 fond = pygame.image.load("foret1.jpg").convert()
-fond = pygame.transform.smoothscale(fond,(fond.get_width()//5,fond.get_height()//5)) 
+fond = pygame.transform.smoothscale(fond,(fond.get_width()//4,fond.get_height()//4)) 
 fenetre = pygame.display.set_mode(fond.get_size(),RESIZABLE)
 
 fenetre.blit(fond,(0,0))
@@ -16,7 +32,7 @@ fenetre.blit(fond,(0,0))
 perso = pygame.image.load("dino.png").convert_alpha() 
 perso = pygame.transform.smoothscale(perso,(fond.get_width()//5,fond.get_height()//5))
 perso_x=50
-perso_y=550
+perso_y=680
 
 fenetre.blit(perso, (perso_x,perso_y))
 
@@ -27,13 +43,14 @@ while continuer:
 
 	pygame.time.Clock().tick(30)
 
+
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			continuer = 0
 
 		elif event.type == KEYDOWN:
 			if event.key == K_SPACE:
-				continuer=1
+				continuer=1	
 
 
 	pygame.display.flip()
